ac8e4f05695aeefb60859acc3edc59cc50235fe0
Andrea Baldo <andrixb@gmail.com>
2016-05-03 15:57:13 +0200

    issue on db

--- app.js
+++ app.js
@@ -2,60 +2,60 @@
  * Module dependencies
  */
 
- var Backbone = require( 'backbone' );
- var _ = require( 'underscore' );
- var $ = require( 'jquery' );
- var fs = require( 'fs' );
+var Backbone = require('backbone');
+var _ = require('underscore');
+var $ = require('jquery');
+var fs = require('fs');
 
-var projects = require( './projects' );
+var projects = require('./projects');
 // var home = require('./home');
 
 var HomeView = Backbone.View.extend({
-  tagName: 'div',
+    tagName: 'div',
 
-  // className: 'hi',
+    className: 'container',
 
-  // events: {
-  //   'click #project-btn': 'showProjects'
-  // },
+    events: {
+        'click #project-btn': 'showProjects'
+    },
 
-  // showProjects: function() {
-  //   location.href = "#projects" ;
-  //   // this.render();
-  // },
+    showProjects: function() {
+        location.href = "#projects";
+        // this.render();
+    },
 
-  initialize: function() {
-    $( "#root" ).html( this.el );
-    this.render();
-  },
+    initialize: function() {
+        $("#root").html(this.el);
+        this.render();
+    },
 
-  template: _.template( fs.readFileSync( './templates/home.html', 'utf8' ) ),
+    template: _.template(fs.readFileSync('./templates/home.html', 'utf8')),
 
-  render: function() {
-    this.$el.html( this.template() );  //don't get why it needs to have an empty template call
-    return this;
-  }
+    render: function() {
+        this.$el.html(this.template());
+        return this;
+    }
 });
 
- var Router = Backbone.Router.extend({
+var Router = Backbone.Router.extend({
 
-   routes: {
-     "": "home",
-     "projects":  "projects"    // #projects
-   },
+    routes: {
+        "": "home",
+        "projects": "projects" // #projects
+    },
 
-   projects:projects.list,
+    projects: projects.list,
 
-   home: function () {
-     this.view = new HomeView();
-   }
+    home: function() {
+        this.view = new HomeView();
+    }
 
 });
 
 var router = new Router;
 Backbone.history.start();
 
-Backbone.sync = function( method, model, options ) {
+Backbone.sync = function(method, model, options) {
 
-console.log( 'sync', arguments.length )
+    console.log('sync', arguments.length)
 };
--- projects/index.js
+++ projects/index.js
@@ -6,19 +6,19 @@ var api = require( '../api' );
 
 var ProjectModel = Backbone.Model.extend({
   defaults: {
-    name: 'woof'
+    name: '',
+    description: ''
   }
-})
+});
 
 var ProjectsCollection = Backbone.Collection.extend({
   model: ProjectModel
 });
 
-
 var ProjectsView = Backbone.View.extend({
   tagName: 'div',
 
-  className: 'projects',
+  className: 'container',
 
   collection: new ProjectsCollection(),
 
@@ -35,7 +35,7 @@ var ProjectsView = Backbone.View.extend({
   // },
 
   initialize: function() {
-    // this.collection.add(api.getProjects()); //returns array
+    // this.collection.add( api.getProjects() ); //returns array
     $( "#root" ).html( this.el );
     this.render();
   },
@@ -43,13 +43,10 @@ var ProjectsView = Backbone.View.extend({
   template: _.template( fs.readFileSync( __dirname + '/../templates/projects.html', 'utf8' ) ),
 
   render: function() {
-    this.$el.html( this.template({ projects: this.collection.models }) );
+    this.$el.html( this.template( { projects: this.collection.models } ) );
     return this;
-
-    // projects: this.collection.models
   }
-})
-
+});
 
 exports.list = function () {
   this.view = new ProjectsView();
